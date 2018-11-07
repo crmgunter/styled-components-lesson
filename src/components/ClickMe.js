88276ead50bb06ff0f10da1e67b61cdaf9e9acde
crmgunter <crmgunter@gmail.com>
2018-11-06 23:17:55 -0500

    extended button component to override styles using styled components

--- src/components/ClickMe.js
+++ src/components/ClickMe.js
@@ -17,12 +17,41 @@ const Button = styled.a`
   }
 `
 
+const ErrorButton = styled(Button)`
+  background-color: #e74c3c;
+  box-shadow: 0px 5px 0px 0px #CE3323;
+  &:hover{
+    background-color: #FF6656;
+  }
+`;
+
+const SuccessButton = styled(Button)`
+  background-color: #2ecc71;
+  box-shadow: 0px 5px 0px 0px #15B358;
+  &:hover{
+    background-color: #48E68B;
+  }
+`;
+
+const InfoButton = styled(Button)`
+  background-color: #f1c40f;
+  box-shadow: 0px 5px 0px 0px #D8AB00;
+  &:hover{
+    background-color: #FFDE29;
+  }
+`
+
+
+
 class ClickMe extends Component {
     render() {
 
         return (
             <div>
                 <Button>Click me!</Button>
+                <ErrorButton>Error</ErrorButton>
+                <SuccessButton>Success</SuccessButton>
+                <InfoButton>Info</InfoButton>
             </div>
         )
     }
